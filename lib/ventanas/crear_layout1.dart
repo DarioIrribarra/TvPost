90ad57255d2d7cecbbb613ad3ae978a95d809529
Dario Irribarra Bello <irribarraster@gmail.com>
2020-10-02 22:18:20 -0300

    Commit desde vscode

--- lib/ventanas/crear_layout1.dart
+++ lib/ventanas/crear_layout1.dart
@@ -9,7 +9,7 @@ class CrearLayout1 extends StatefulWidget {
 }
 
 class _CrearLayout1State extends State<CrearLayout1> {
-
+  //Acá va el cambio desde VSCode
   //Acá va el cambio desde Android
 
   //dato que indica que parte del layout se quiere modificar y se envia a la
@@ -19,101 +19,94 @@ class _CrearLayout1State extends State<CrearLayout1> {
   Widget widget1;
   bool _visible = false;
 
-
-    @override
+  @override
   Widget build(BuildContext context) {
-      //Si ya se seleccionó un archivo de media se crea un nuevo layout
-      if (DatosEstaticos.wiget1!=null){
-        widget1 = DatosEstaticos.wiget1;
-      } else {
-        Image _imageSeleccionLayout = Image.asset('imagenes/layout1a.png');
-        widget1 = _imageSeleccionLayout;
-      }
-      return Scaffold(
-        //Appbar viene de archivo custom_widgets.dart
-        appBar: CustomAppBar(),
-        body: SingleChildScrollView(
-            child: Container(
-              child: Column(
-                children: [
-                  GestureDetector(
-                    onTap: (){
-                      setState(() {
-                        if (!_visible){
-                          _visible = true;
-                        }
-                        divisionLayout = '1-1';
-                      });
-                    },
-                    child: Container(
-                      height: 300,
-                        width: MediaQuery.of(context).size.width,
-                        child: Center(child: widget1)
+    //Si ya se seleccionó un archivo de media se crea un nuevo layout
+    if (DatosEstaticos.wiget1 != null) {
+      widget1 = DatosEstaticos.wiget1;
+    } else {
+      Image _imageSeleccionLayout = Image.asset('imagenes/layout1a.png');
+      widget1 = _imageSeleccionLayout;
+    }
+    return Scaffold(
+      //Appbar viene de archivo custom_widgets.dart
+      appBar: CustomAppBar(),
+      body: SingleChildScrollView(
+          child: Container(
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                setState(() {
+                  if (!_visible) {
+                    _visible = true;
+                  }
+                  divisionLayout = '1-1';
+                });
+              },
+              child: Container(
+                  height: 300,
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(child: widget1)),
+            ),
+            Visibility(
+              visible: _visible,
+              child: Container(
+                margin: EdgeInsets.only(left: 115.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {
+                            //Va a la otra ventana esperando respuesta
+                            navegarYEsperarRespuesta('/seleccionar_imagen');
+                          },
+                          child: Text('Imagen'),
+                        ),
+                        RaisedButton(
+                          onPressed: () {
+                            //Va a la otra ventana esperando respuesta
+                            navegarYEsperarRespuesta('/seleccionar_video');
+                          },
+                          child: Text('Video'),
+                        ),
+                      ],
                     ),
-                  ),
-                  Visibility(
-                    visible: _visible,
-                    child: Container(
-                      margin: EdgeInsets.only(left: 115.0),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              RaisedButton(
-                                onPressed: () {
-                                  //Va a la otra ventana esperando respuesta
-                                  navegarYEsperarRespuesta('/seleccionar_imagen');
-                                },
-                                child: Text('Imagen'),
-                              ),
-                              RaisedButton(
-                                onPressed: (){
-                                  //Va a la otra ventana esperando respuesta
-                                  navegarYEsperarRespuesta('/seleccionar_video');
-                                },
-                                child: Text('Video'),
-                              ),
-                            ],
-                          ),
-                          Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            children: [
-                              RaisedButton(
-                                onPressed: (){},
-                                child: Text('Url'),
-                              ),
-                              RaisedButton(
-                                onPressed: (){},
-                                child: Text('Crear'),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Text('Url'),
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Text('Crear'),
+                        ),
+                      ],
                     ),
-                  )
-                ],
+                  ],
+                ),
               ),
             )
+          ],
         ),
-      );
-
+      )),
+    );
   }
 
-  navegarYEsperarRespuesta(String rutaVentana) async{
-    final result = await Navigator.pushNamed(context,
-        rutaVentana,
-        arguments: {
-          'division_layout': divisionLayout,
-        });
-    if (result!=null){setState(() {
-
-    });}
+  navegarYEsperarRespuesta(String rutaVentana) async {
+    final result = await Navigator.pushNamed(context, rutaVentana, arguments: {
+      'division_layout': divisionLayout,
+    });
+    if (result != null) {
+      setState(() {});
+    }
   }
 }
-
 
 /*class CrearLayout1 extends StatelessWidget {
   //Datos que se envía completo desde la ventana de selección de media
